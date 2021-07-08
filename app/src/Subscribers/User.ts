a7054f79a969750966265a7452456de2ee79811c
Christoffer <dev@kodemon.net>
2021-07-08 20:11:54 +0900

    Remove stray logs

--- app/src/Subscribers/User.ts
+++ app/src/Subscribers/User.ts
@@ -16,14 +16,12 @@ publisher.subscribe([
   new EventSubscriber(UserNameSet, async ({ data }) => {
     const user = User.findBy("id", data.id);
     if (user) {
-      console.log({ name: data.name });
       user.update({ name: data.name });
     }
   }),
   new EventSubscriber(UserEmailSet, async ({ data }) => {
     const user = User.findBy("id", data.id);
     if (user) {
-      console.log({ email: data.email });
       user.update({ email: data.email });
     }
   })
